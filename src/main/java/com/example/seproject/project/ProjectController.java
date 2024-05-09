7e097d799d5ac7f80b415a1ba2ce8bdeb415ad26
OH-GITAEK <dhrlxor1512@gmail.com>
2024-05-09 13:02:19 +0900

    [feat] project service계층 생성

--- src/main/java/com/example/seproject/project/ProjectController.java
+++ src/main/java/com/example/seproject/project/ProjectController.java
@@ -3,7 +3,6 @@ package com.example.seproject.project;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -11,16 +10,16 @@ import java.util.List;
 @RestController
 public class ProjectController {
     @Autowired
-    private ProjectRepository projectRepository;
+    private ProjectService projectService;
 
     @GetMapping("/api/projects")
     public List<Project> list(){
-        return projectRepository.findAll();
+        return projectService.getList();
     }
 
     @GetMapping("/api/projects/{id}")
     public Project detail(@PathVariable Integer id){
-        return projectRepository.findById(id).orElse(null);
+        return projectService.getProject(id);
     }
 
 }
---
+++ src/main/java/com/example/seproject/project/ProjectService.java
@@ -0,0 +1,21 @@
+package com.example.seproject.project;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class ProjectService {
+    private final ProjectRepository projectRepository;
+
+    public List<Project> getList(){
+        return projectRepository.findAll();
+    }
+
+    public Project getProject(Integer id){
+        return projectRepository.findById(id).orElse(null);
+    }
+}
