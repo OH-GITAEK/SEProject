6e4d4f5c7cca1d2831ec4faf329a58bccb0425da
OH-GITAEK <dhrlxor1512@gmail.com>
2024-05-09 22:30:31 +0900

    [feat] create기ëate 기능 추가 및 유효성 검사 추가

--- src/main/java/com/example/seproject/project/ProjectController.java
+++ src/main/java/com/example/seproject/project/ProjectController.java
@@ -1,8 +1,12 @@
 package com.example.seproject.project;
 
+import jakarta.validation.Valid;
+import jakarta.validation.ValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,4 +26,11 @@ public class ProjectController {
         return projectService.getProject(id);
     }
 
+    @PostMapping("api/project/create")
+    public Project create(@Valid ProjectForm projectForm, BindingResult bindingResult){
+        if (bindingResult.hasErrors()) {
+            throw new ValidationException("Validation failed");
+        }
+        return projectService.create(projectForm.getProjectTitle(), projectForm.getProjectDescription());
+    }
 }
--- src/main/java/com/example/seproject/project/ProjectService.java
+++ src/main/java/com/example/seproject/project/ProjectService.java
@@ -1,6 +1,7 @@
 package com.example.seproject.project;
 
 import java.util.List;
+import java.time.LocalDateTime;
 
 import org.springframework.stereotype.Service;
 
@@ -11,11 +12,19 @@ import lombok.RequiredArgsConstructor;
 public class ProjectService {
     private final ProjectRepository projectRepository;
 
-    public List<Project> getList(){
+    public List<Project> getList() {
         return projectRepository.findAll();
     }
 
-    public Project getProject(Long id){
+    public Project getProject(Long id) {
         return projectRepository.findById(id).orElse(null);
+    }
+
+    public Project create(String ProjectTitle, String projectDescription) {
+        Project q = new Project();
+        q.setProjectTitle(ProjectTitle);
+        q.setProjectDescription(projectDescription);
+        q.setReportedDate(LocalDateTime.now());
+        return projectRepository.save(q);
     }
 }
