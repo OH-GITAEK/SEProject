44cce5db1d721eee011ad5643fffeacb422e7d08
LCW <cksdn43@cau.ac.kr>
2024-05-12 05:25:15 +0900

    [build] 실행파일변경

--- src/main/fronted/src/App.js
+++ src/main/fronted/src/App.js
@@ -18,10 +18,10 @@ function App() {
               <div className="logo">Team5</div>
               <div className="login">
                   <form action="/member/login" method="post">
-                      아이디: <input type="text" name="memberEmail"/> <br/>
-                      비밀번호: <input type="password" name="memberPassword"/> <br/>
-                      <input type="submit" value="로그인"/>
-                      <a href="/member/save">회원가입</a>
+                      ID: <input type="text" name="memberEmail"/> <br/>
+                      PASSWORD: <input type="password" name="memberPassword"/> <br/>
+                      <input type="submit" value="Sign in"/>
+                      <a id="signup" href="/member/save">Sign up</a>
                   </form>
               </div>
               <div className="menu">
