7b5d86bfc78b9f72bfe6b4103ec69d83d7248136
LCW <cksdn43@cau.ac.kr>
2024-05-12 17:55:20 +0900

    [fix] 로그인 기능 먹통 해결했습니다.

--- src/main/fronted/src/App.js
+++ src/main/fronted/src/App.js
@@ -19,51 +19,13 @@ function App() {
 
   return (
       <div className="App">
-              <BrowserRouter>
-                  <MenuBar/>
-                  <Routes>
-                      <Route path="/" element={<Board/>} />
-                      <Route path="/board" element={<BoardDetail/>} />
-                  </Routes>
-              </BrowserRouter>
-          <div className="sidebar">
-              <div className="logo">Team5</div>
-              <div className="login">
-                  <form action="/member/login" method="post">
-                      ID: <input type="text" name="memberEmail"/> <br/>
-                      PASSWORD: <input type="password" name="memberPassword"/> <br/>
-                      <input type="submit" value="Sign in"/>
-                      <a id="signup" href="/member/save">Sign up</a>
-                  </form>
-              </div>
-              <div className="menu">
-                  <a href="/">Home</a>
-                  <a href="#">Issue</a>
-                  <a href="#">Analysis</a>
-              </div>
-          </div>
-          {/*content를 나중에 component로 변경바람*/}
-          <div className="root">
-              <div className="table">
-                  <div className="head">
-                      <div className="row">
-                          <div className="cell">Title</div>
-                          <div className="cell">Description</div>
-                          <div className="cell">Reporter</div>
-                          <div className="cell">ReportedDate</div>
-                          <div className="cell">Comment</div>
-                          <div className="cell">History</div>
-                      </div>
-                  </div>
-                  <div className="body">
-                      <div className="cell">{hello}</div>
-                      <div className="cell">{hello}</div>
-                      <div className="cell">{hello}</div>
-                      <div className="cell">{hello}</div>
-                      <div className="cell">{hello}</div>
-                  </div>
-              </div>
-          </div>
+          <BrowserRouter>
+              <MenuBar/>
+              <Routes>
+                  <Route path="/" element={<Board/>} />
+                  <Route path="/board" element={<BoardDetail/>} />
+              </Routes>
+          </BrowserRouter>
       </div>
   );
 }
--- src/main/fronted/src/Components/MenuBar.js
+++ src/main/fronted/src/Components/MenuBar.js
@@ -1,27 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Login from "./Login.js";
 
-
+// 메뉴바 상태를 확인하고 반환하는 함수
 function MenuBar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(true);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
 
     return (
-        <div className="sidebar">
-            <div className="logo">Team5</div>
-            <div className="login">
-                <form action="/member/login" method="post">
-                    아이디: <input type="text" name="memberEmail"/> <br/>
-                    비밀번호: <input type="password" name="memberPassword"/> <br/>
-                    <input type="submit" value="로그인"/>
-                    <a href="/member/save">회원가입</a>
-                </form>
+        <div>
+             {/* 메뉴 아이콘의 위치를 조건에 따라 변경 */}
+             <div className={`bar-toggle ${isMenuOpen ? "next-to-sidebar" : "top-left"}`} onClick={toggleMenu}>
+                <span>☰</span> {/* 메뉴 아이콘 */}
             </div>
-            <div className="menu">
-                <a href="/">Home</a>
-                <a href="#">Issue</a>
-                <a href="#">Analysis</a>
-            </div>
+        {isMenuOpen && (
+                <div className="sidebar">
+                    <div className="logo">Team5</div>
+                    <Login/>
+                    <div className="menu">
+                        <a href="/">Home</a>
+                        <a href="#">Issue</a>
+                        <a href="#">Analysis</a>
+                        </div>
+                </div>
+        )}
         </div>
     );
-
 }
 
 export default MenuBar;
