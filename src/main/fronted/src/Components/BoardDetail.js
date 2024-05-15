6713f4c9c790d80ae73da532de3b0d88b0dcc4ca
roxasventus <magicwand10@naver.com>
2024-05-15 17:30:11 +0900

    [feat] : 이슈 추가 및 삭제

--- src/main/fronted/src/Components/BoardDetail.js
+++ src/main/fronted/src/Components/BoardDetail.js
@@ -10,7 +10,7 @@ function BoardDetail() {
                     게시글 내용
                 </p>
                 <div className="buttons">
-                    <button className="button">추가</button>
+                    <button className="button">수정</button>
                     <button className="button">삭제</button>
                     <button className="button">해결</button>
                 </div>
