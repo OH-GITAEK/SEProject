01d21e8c459701e5bcde398f0e97e6c6f25d6c26
OH-GITAEK <dhrlxor1512@gmail.com>
2024-06-02 16:23:10 +0900

    [fix] recomend err

--- src/main/java/com/example/seproject/project/ProjectRepository.java
+++ src/main/java/com/example/seproject/project/ProjectRepository.java
@@ -1,6 +1,11 @@
 package com.example.seproject.project;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface ProjectRepository extends JpaRepository<Project,Long>{
 
+import java.util.Optional;
+
+public interface ProjectRepository extends JpaRepository<Project,Long>{
+    @EntityGraph(attributePaths = {"devUser", "issues", "issues.assignee", "issues.fixer", "issues.keyWords"})
+    Optional<Project> findWithDetailsById(Long id);
 }
